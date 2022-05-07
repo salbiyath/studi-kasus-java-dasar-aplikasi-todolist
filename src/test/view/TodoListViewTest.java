2c56ff4ea6158f1f287af761d74013427f8e4931
Muhammad Salbiyath <salbiyat@gmail.com>
2022-05-07 22:33:57 +0700

    Membuat Test view remove todolist

--- src/test/view/TodoListViewTest.java
+++ src/test/view/TodoListViewTest.java
@@ -8,7 +8,7 @@ import view.TodoListView;
 
 public class TodoListViewTest {
     public static void main(String[] args) {
-        testAddTodoList();
+        testRemoveTodoList();
     }
 
     public static void testShowTodoList(){
@@ -36,5 +36,21 @@ public class TodoListViewTest {
 
         todoListService.showTodoList();
 
+    }
+
+    public static void testRemoveTodoList(){
+        TodoListRepository todoListRepository = new TodoListRepositoryImpl();
+        TodoListService todoListService = new TodoListServieImpl(todoListRepository);
+        TodoListView todoListView = new TodoListView(todoListService);
+
+        todoListService.addTodoList("Belajar Java Dasar");
+        todoListService.addTodoList("Belajar Java OOP");
+        todoListService.addTodoList("Belajar Java Standard Classes");
+
+        todoListService.showTodoList();
+
+        todoListView.removeTodoList();
+
+        todoListService.showTodoList();
     }
 }
